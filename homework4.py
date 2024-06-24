43b5132ca52c127331cd7da5a9ed0a308d6014e3
Katya <86482797+Baytszy@users.noreply.github.com>
2024-06-24 16:12:33 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+my_string = input("Введите ваше имя ")
+print("Здравствуйте,", my_string)
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string[0])
+print(my_string[-1])
+print('Привет, я строка, пока'.replace(' ','#'))
